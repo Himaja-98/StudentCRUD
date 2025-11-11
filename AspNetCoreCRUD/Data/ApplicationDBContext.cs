ac774746493a7c7519520b56c4cc8829f27ca314
satya <satya@LAPTOP-0KKTPE3J>
2025-11-11 20:05:11 +0530

    Add project files.

---
+++ AspNetCoreCRUD/Data/ApplicationDBContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.EntityFrameworkCore;
+using StudentappCRUD.Models;
+
+namespace StudentappCRUD.Data
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Student> students { get; set; }
+
+    }
+}
---
+++ AspNetCoreCRUD/Models/Student.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StudentappCRUD.Models
+{
+    public class Student
+    {
+        [Key]
+        [Display(Name="Student ID")]
+        public int StudnetId { get; set; }
+
+        [Required]
+        [Display(Name="Student Name")]
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Course { get; set; }
+
+        [Display(Name="Enrollment Date")]
+        public DateTime EnrollmentDate { get; set; }
+    }
+}
